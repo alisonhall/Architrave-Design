8ca2d7cec610245fe1c21952a0f81f733a623feb
Alison Hall <alisonh0101@gmail.com>
2019-10-20 15:09:04 -0400

    Add transformations to cloudinary urls

--- src/pages/portfolio/renovations-additions/princess-margaret-facelift.js
+++ src/pages/portfolio/renovations-additions/princess-margaret-facelift.js
@@ -14,7 +14,7 @@ const PrincessMargaretFacelift = (props) => (
     <section className="contentWrapper layoutAll layout1">
       <ImageTile dataItem={{
         num: '1',
-        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/v1571329784/ArchitraveDesign/Princess-Margaret-Transitional/Princess_Margaret_Transitional1_yp7fuo.jpg',
+        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/f_auto,q_auto/v1571329784/ArchitraveDesign/Princess-Margaret-Transitional/Princess_Margaret_Transitional1_yp7fuo.jpg',
         width: 'calc(100% - (1.5% * 2))',
         height: 'calc(500px * 0.9)',
         backgroundPosition: '50% 30%'
@@ -25,14 +25,14 @@ const PrincessMargaretFacelift = (props) => (
       }} />
       <ImageTile dataItem={{
         num: '2',
-        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/v1571329783/ArchitraveDesign/Princess-Margaret-Transitional/Princess_Margaret_Before_sco67f.jpg',
+        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/f_auto,q_auto/v1571329783/ArchitraveDesign/Princess-Margaret-Transitional/Princess_Margaret_Before_sco67f.jpg',
         width: 'calc(50% - (1.5% * 2))',
         height: 'calc(500px * 0.75)',
         textOverlay: 'Before'
       }} />
       <ImageTile dataItem={{
         num: '3',
-        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/v1571329784/ArchitraveDesign/Princess-Margaret-Transitional/Princess_Margaret_Transitional2_j8wr8i.jpg',
+        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/f_auto,q_auto/v1571329784/ArchitraveDesign/Princess-Margaret-Transitional/Princess_Margaret_Transitional2_j8wr8i.jpg',
         width: 'calc(50% - (1.5% * 2))',
         height: 'calc(500px * 0.75)'
       }} />
