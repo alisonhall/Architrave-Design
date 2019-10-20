d4c03d4be9910fb8e9c8582b87ce575b3bff3779
Alison Hall <alisonh0101@gmail.com>
2019-10-20 13:17:52 -0400

    Update Royal York Facelift page

--- src/pages/portfolio/renovations-additions/royal-york-facelift.js
+++ src/pages/portfolio/renovations-additions/royal-york-facelift.js
@@ -2,8 +2,14 @@ import React from "react"
 
 import Layout from "../../../components/layout"
 import SEO from "../../../components/seo"
-import dataItemSwitcher from "../../../components/dataItemSwitcher";
-import PrevNextProjectLink from "../../../components/prevNextProjectLink"
+import ImageLayout1Col1Row from '../../../components/imageLayout1Col1Row';
+import ImageTile from '../../../components/imageTile';
+import TextBlurb from '../../../components/textBlurb';
+import TextBlurbFiller from '../../../components/textBlurbFiller';
+import PrevNextProjectLink from '../../../components/prevNextProjectLink';
+
+import image1 from '../../../images/Renovations-Additions/Royal-York-1.jpg';
+import image2 from '../../../images/Renovations-Additions/Royal-York-2.jpg';
 
 const data = [
   {
@@ -26,7 +32,23 @@ const RoyalYorkFacelift = (props) => (
   <Layout urlPath={props.location.pathname} mainClasses="portfolio royalYorkFacelift">
     <SEO />
     <section class="contentWrapper layoutAll layout1">
-      {data.map(item => dataItemSwitcher(item))}
+      <ImageTile dataItem={{
+        num: '1',
+        image: image1,
+        width: 'calc(50% - (1.5% * 2))',
+        height: 'calc(500px * 0.75)',
+        textOverlay: 'Before'
+      }} />
+      <ImageTile dataItem={{
+        num: '2',
+        image: image2,
+        width: 'calc(50% - (1.5% * 2))',
+        height: 'calc(500px * 0.75)',
+        textOverlay: 'New Facade'
+      }} />
+      <TextBlurb dataItem={{
+        title: 'Royal York Facelift',
+      }} />
       <PrevNextProjectLink direction="previous" linkUrl="" />
       <PrevNextProjectLink direction="next" linkUrl="" />
     </section>
