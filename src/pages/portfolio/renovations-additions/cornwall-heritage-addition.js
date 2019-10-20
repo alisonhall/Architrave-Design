dacfa0255ceed64a4d815bc1a67b7b2a9d72aae6
Alison Hall <alisonh0101@gmail.com>
2019-10-20 12:36:03 -0400

    Update Cornwall Heritage page

--- src/pages/portfolio/renovations-additions/cornwall-heritage-addition.js
+++ src/pages/portfolio/renovations-additions/cornwall-heritage-addition.js
@@ -2,50 +2,63 @@ import React from "react"
 
 import Layout from "../../../components/layout"
 import SEO from "../../../components/seo"
-import dataItemSwitcher from "../../../components/dataItemSwitcher";
-import PrevNextProjectLink from "../../../components/prevNextProjectLink"
+import ImageLayout1Col1Row from '../../../components/imageLayout1Col1Row';
+import ImageTile from '../../../components/imageTile';
+import TextBlurb from '../../../components/textBlurb';
+import TextBlurbFiller from '../../../components/textBlurbFiller';
+import PrevNextProjectLink from "../../../components/prevNextProjectLink";
 
-const data = [
-  {
-    type: 'image-tile',
-    num: '1'
-  },
-  {
-    type: 'text-blurb',
-    title: "Loyalist Farmhouse Addition",
-    text: "A modern addition to this charming 1820’s home mirrors it’s proportions but more than doubles the living space. The ground floor is a country kitchen, topped by a vaulted master with sweeping views."
-  },
-  {
-    type: 'image-tile',
-    num: '2'
-  },
-  {
-    type: 'image-tile',
-    num: '3'
-  },
-  {
-    type: 'image-tile',
-    num: '4'
-  },
-  {
-    type: 'image-tile',
-    num: '5'
-  },
-  {
-    type: 'image-tile',
-    num: '6'
-  },
-  {
-    type: 'image-tile',
-    num: '7'
-  }
-]
+import image1 from '../../../images/Renovations-Additions/Cornwall-1.jpg';
+import image2 from '../../../images/Renovations-Additions/Cornwall-2.jpg';
+import image3 from '../../../images/Renovations-Additions/Cornwall-3.jpg';
+import image4 from '../../../images/Renovations-Additions/Cornwall-4.jpg';
+import image5 from '../../../images/Renovations-Additions/Cornwall-5.jpg';
+import image6 from '../../../images/Renovations-Additions/Cornwall-6.jpg';
 
 const ContactUs = (props) => (
   <Layout urlPath={props.location.pathname} mainClasses="portfolio cornwallHeritageAddition">
     <SEO />
     <section class="contentWrapper layoutAll layoutRenovationsCornwallHeritageAddition">
-      {data.map(item => dataItemSwitcher(item))}
+      <ImageTile dataItem={{
+        num: '1',
+        image: image1,
+        width: 'calc(100% - (1.5% * 2))',
+        height: 'calc(500px * 1.24)'
+      }} />
+      <TextBlurb dataItem={{
+        title: 'Loyalist Farmhouse Addition',
+        text: 'A modern addition to this charming 1820’s home mirrors it’s proportions but more than doubles the living space. The ground floor is a country kitchen, topped by a vaulted master with sweeping views.'
+      }} />
+      <ImageTile dataItem={{
+        num: '2',
+        image: image2,
+        width: 'calc(62% - (1.5% * 2))',
+        height: 'calc(500px * 1.62)'
+      }} />
+      <ImageTile dataItem={{
+        num: '3',
+        image: image3,
+        width: 'calc(38% - (1.5% * 2))',
+        height: 'calc(500px * 0.97)'
+      }} />
+      <ImageTile dataItem={{
+        num: '4',
+        image: image4,
+        width: 'calc(38% - (1.5% * 2))',
+        height: 'calc(500px * 0.58)'
+      }} />
+      <ImageTile dataItem={{
+        num: '5',
+        image: image5,
+        width: 'calc(62% - (1.5% * 2))',
+        height: 'calc(500px * 0.7)'
+      }} />
+      <ImageTile dataItem={{
+        num: '6',
+        image: image6,
+        width: 'calc(38% - (1.5% * 2))',
+        height: 'calc(500px * 0.7)'
+      }} />
       <PrevNextProjectLink direction="previous" linkUrl="/portfolio/renovations-additions/etobicoke-renewal" />
       <PrevNextProjectLink direction="next" linkUrl="/portfolio/renovations-additions/rosedale-revival" />
     </section>
