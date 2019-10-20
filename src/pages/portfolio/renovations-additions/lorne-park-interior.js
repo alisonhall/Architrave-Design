4cd63dc6e2e29d65c2ef1d87f1fc7367ddb33fbb
Alison Hall <alisonh0101@gmail.com>
2019-10-20 12:52:53 -0400

    Update Lorne Park page

--- src/pages/portfolio/renovations-additions/lorne-park-interior.js
+++ src/pages/portfolio/renovations-additions/lorne-park-interior.js
@@ -2,42 +2,59 @@ import React from "react"
 
 import Layout from "../../../components/layout"
 import SEO from "../../../components/seo"
-import dataItemSwitcher from "../../../components/dataItemSwitcher";
-import PrevNextProjectLink from "../../../components/prevNextProjectLink"
+import ImageLayout1Col1Row from '../../../components/imageLayout1Col1Row';
+import ImageTile from '../../../components/imageTile';
+import TextBlurb from '../../../components/textBlurb';
+import TextBlurbFiller from '../../../components/textBlurbFiller';
+import PrevNextProjectLink from "../../../components/prevNextProjectLink";
 
-const data = [
-  {
-    type: 'image-tile',
-    num: '1'
-  },
-  {
-    type: 'image-tile',
-    num: '2'
-  },
-  {
-    type: 'image-tile',
-    num: '3'
-  },
-  {
-    type: 'text-blurb',
-    title: "Lorne Park Interior",
-    text: "Updating the living area of this Lorne Park Prairie Style home is the first phase of a long term plan to remake the entire home. A dark and cluttered living-dining area was gutted, and redone with a light and modern touch."
-  },
-  {
-    type: 'image-tile',
-    num: '4'
-  },
-  {
-    type: 'image-tile',
-    num: '5'
-  }
-]
+import image1 from '../../../images/Renovations-Additions/Lorne-Park-2.jpg';
+import image2 from '../../../images/Renovations-Additions/Lorne-Park-4.jpg';
+import image3 from '../../../images/Renovations-Additions/Lorne-Park-1.jpg';
+import image4 from '../../../images/Renovations-Additions/Lorne-Park-5.jpg';
+import image5 from '../../../images/Renovations-Additions/Lorne-Park-6.jpg';
 
 const ContactUs = (props) => (
   <Layout urlPath={props.location.pathname} mainClasses="portfolio lorneParkInterior">
     <SEO />
     <section class="contentWrapper layoutAll layoutRenovationsLorneParkInterior">
-      {data.map(item => dataItemSwitcher(item))}
+      <ImageTile dataItem={{
+        num: '1',
+        image: image1,
+        width: 'calc(23% - (1.5% * 2))',
+        height: 'calc(500px * 0.45)'
+      }} />
+      <ImageTile dataItem={{
+        num: '2',
+        image: image2,
+        width: 'calc(77% - (1.5% * 2))',
+        height: 'calc(500px * 0.96)',
+        float: 'right'
+      }} />
+      <ImageTile dataItem={{
+        num: '3',
+        image: image3,
+        width: 'calc(23% - (1.5% * 2))',
+        height: 'calc(500px * 0.45)'
+      }} />
+      <TextBlurb dataItem={{
+        title: 'Lorne Park Interior',
+        text: 'Updating the living area of this Lorne Park Prairie Style home is the first phase of a long term plan to remake the entire home. A dark and cluttered living-dining area was gutted, and redone with a light and modern touch.'
+      }} />
+      <ImageTile dataItem={{
+        num: '4',
+        image: image4,
+        width: 'calc(50% - (1.5% * 2))',
+        height: 'calc(500px * 0.52)',
+        backgroundPosition: '50% 30%'
+      }} />
+      <ImageTile dataItem={{
+        num: '5',
+        image: image5,
+        width: 'calc(50% - (1.5% * 2))',
+        height: 'calc(500px * 0.52)',
+        backgroundPosition: '50% 30%'
+      }} />
       <PrevNextProjectLink direction="previous" linkUrl="/portfolio/renovations-additions/rosedale-revival" />
       <PrevNextProjectLink direction="next" linkUrl="/portfolio/renovations-additions/lytton-park-update" />
     </section>
