60bd2d76678dca61cda41200be2bf1aba566d119
Alison Hall <alisonh0101@gmail.com>
2019-10-20 13:44:07 -0400

    Update some of Upcoming page

--- src/pages/portfolio/upcoming.js
+++ src/pages/portfolio/upcoming.js
@@ -2,30 +2,48 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import ImageLayout1Col1Row from '../../components/imageLayout1Col1Row';
+import ImageLinkTile from "../../components/imageLinkTile";
+import ImageFillerTile from '../../components/imageFillerTile';
+import ImageTile from '../../components/imageTile';
+import TextBlurb from '../../components/textBlurb';
+
+import image1 from '../../images/Upcoming/Oakville-New-Home-01.jpg';
+import image2 from '../../images/Upcoming/StGeorges-GC.jpg';
+import image3 from '../../images/Upcoming/King-Georges6.jpg';
+import image4 from '../../images/Upcoming/Before-StGeorges.jpg';
 
 const Upcoming = (props) => (
   <Layout urlPath={props.location.pathname} mainClasses="portfolio upcoming">
     <SEO />
     <section className="contentWrapper layoutAll layoutUpcoming">
-      <div className="image image2 clearfix">
-        <div className="shadowOverlay clearfix">
-          <div className="imageDiv imageDiv2"></div>
-        </div>
-      </div>
-      <div className="image image4 clearfix">
-        <div className="shadowOverlay clearfix">
-          <div className="imageDiv imageDiv4"></div>
-        </div>
-      </div>
+      <ImageTile dataItem={{
+        num: '2',
+        image: image2,
+        width: 'calc(100% - (1.5% * 2))',
+        height: 'calc(500px * 0.8)',
+        backgroundPosition: '50% 80%'
+      }} />
+      <ImageTile dataItem={{
+        num: '4',
+        image: image4,
+        width: 'calc(45% - (1.5% * 2))',
+        height: '170px',
+        backgroundPosition: '50% 80%',
+        float: 'right',
+        marginTop: '-115px'
+      }} />
       <section className="textBlurb">
         <p className="textOverlay">St. George's Rebuild</p>
         <p className="completionDate">Completion 2019</p>
       </section>
-      <div className="image image1 clearfix">
-        <div className="shadowOverlay clearfix">
-          <div className="imageDiv imageDiv1"></div>
-        </div>
-      </div>
+      <ImageTile dataItem={{
+        num: '1',
+        image: image1,
+        width: 'calc(100% - (1.5% * 2))',
+        height: 'calc(500px * 0.8)',
+        backgroundPosition: '50% 80%'
+      }} />
       <section className="textBlurb">
         <p className="textOverlay">Oakville New Home</p>
         <p className="completionDate">Completion 2017</p>
