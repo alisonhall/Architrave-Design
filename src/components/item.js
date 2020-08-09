a6a8d642729c9af46f363f6d24a9cc7f31fbbb78
Alison Hall <ahall7@sapient.com>
2020-08-09 18:32:13 -0400

    Create a generic item which determines components based on provided props

---
+++ src/components/item.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+import ImageTile from './imageTile';
+import ImageLinkTile from './imageLinkTile';
+import ImageFillerTile from './imageFillerTile';
+import TextBlurb from './textBlurb';
+import TextBlurbFiller from './textBlurbFiller';
+
+export default (data) => {
+  const {
+    image,
+    link,
+    text
+  } = data;
+
+  if (image && link) {
+    return <ImageLinkTile {...data} />;
+  } else if (image) {
+    return <ImageTile {...data} />;
+  } else if (text) {
+    return <TextBlurb {...data} />;
+  }
+  return <TextBlurbFiller {...data} />;
+}
