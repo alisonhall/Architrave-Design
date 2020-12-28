67ad60331dafa30d3e94486cee55cf44f69280f4
Alison Hall <alisonh0101@gmail.com>
2020-12-28 14:11:25 -0500

    Fix telephone number

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const Contact = (props) => (
       <div className="contentBackground clearfix">
         <section className="textBlurb">
           <p><a href="mailto:architravedesign@rogers.com"><i className="fa fa-envelope"></i>architravedesign@rogers.com</a></p>
-          <p><i className="fa fa-phone"></i>Toronto: <a href="tel:4162048881">416-207-8881</a></p>
+          <p><i className="fa fa-phone"></i>Toronto: <a href="tel:4162078881">416-207-8881</a></p>
           <p><i className="fa fa-phone"></i>Mississauga & Oakville: <a href="tel:9055993097">905-599-3097</a></p>
         </section>
       </div>
