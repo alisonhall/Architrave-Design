192e5393fdb8c3943cbaded3c5fafe3d10753d27
Alison Hall <alisonh0101@gmail.com>
2021-05-24 16:41:40 -0400

    Add wide layout to new homes page

--- src/pages/portfolio/new-homes.jsx
+++ src/pages/portfolio/new-homes.jsx
@@ -31,19 +31,49 @@ const tiles = {
       }}
     />
   ),
+  creditRiverManorFiller: (
+    <Item
+      num={3}
+      project={projects.creditRiverManor}
+      isFiller
+      image={{
+        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/v1595347209/ArchitraveDesign/1-New-Homes/3-Credit-River-Manor/10-Main-Stair_bsfa5r.jpg'
+      }}
+    />
+  ),
   creditRiverManor: buildProjectTile(projects.creditRiverManor, 4),
   kingswayTransitional: buildProjectTile(projects.kingswayTransitional, 5),
   oakvilleExecutiveHome: buildProjectTile(projects.oakvilleExecutiveHome, 6),
   classicCentreHall: buildProjectTile(projects.classicCentreHall, 7),
   hoggsHollowFrenchCountry: buildProjectTile(projects.hoggsHollowFrenchCountry, 8),
-  traditionalKingswayPark: buildProjectTile(projects.traditionalKingswayPark, 9, { backgroundPosition: '30% 40% '})
+  traditionalKingswayPark: buildProjectTile(projects.traditionalKingswayPark, 9, { backgroundPosition: '30% 40% ' }),
+  kingswayGeorgianFiller: (
+    <Item
+      num={10}
+      project={projects.kingswayGeorgian}
+      isFiller
+      image={{
+        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/v1595347141/ArchitraveDesign/1-New-Homes/1-Kingsway-Classic-Georgian/5-Main-Hall_txsam1.jpg'
+      }}
+    />
+  ),
+  kingswayGeorgianFiller2: (
+    <Item
+      num={10}
+      project={projects.kingswayGeorgian}
+      isFiller
+      image={{
+        imageUrl: 'https://res.cloudinary.com/alisonkhall/image/upload/v1595347150/ArchitraveDesign/1-New-Homes/1-Kingsway-Classic-Georgian/9-Breakfast-Banquette_plhghs.jpg'
+      }}
+    />
+  ),
 }
 
 
 const NewHomes = (props) => (
   <Layout urlPath={props.location.pathname} mainClasses="newHomesOverview">
     <SEO />
-    <section className="contentWrapper layoutAll layoutNewHomes">
+    <section className="contentWrapper layoutAll layoutNewHomes defaultLayout">
       <Row height={705}>
         <Column width="48%">
           {tiles.hoggsHollowTraditional}
@@ -85,6 +115,56 @@ const NewHomes = (props) => (
           {tiles.hoggsHollowFrenchCountry}
         </Column>
         <Column>
+          {tiles.traditionalKingswayPark}
+        </Column>
+      </Row>
+    </section>
+    <section className="contentWrapper layoutAll layoutHome wideLayout">
+      <Row height={550}>
+        <Column width="48%">
+          {tiles.hoggsHollowTraditional}
+        </Column>
+        <Column>
+          <Row imageHeight={275}>
+            <Column width="37%">
+              {tiles.kingswayGeorgian}
+            </Column>
+            <Column width="21%">
+              {tiles.kingswayTransitionalFiller}
+            </Column>
+            <Column width="42%">
+              {tiles.oakvilleExecutiveHome}
+            </Column>
+          </Row>
+          <Row imageHeight={275}>
+            <Column width="42%">
+              {tiles.creditRiverManor}
+            </Column>
+            <Column width="21%">
+              {tiles.creditRiverManorFiller}
+            </Column>
+            <Column width="37%">
+              {tiles.kingswayTransitional}
+            </Column>
+          </Row>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          {tiles.description}
+        </Column>
+      </Row>
+      <Row height={300}>
+        <Column width="37%">
+          {tiles.classicCentreHall}
+        </Column>
+        <Column width="15%">
+          {tiles.kingswayGeorgianFiller2}
+        </Column>
+        <Column width="15%">
+          {tiles.kingswayGeorgianFiller}
+        </Column>
+        <Column width="33%">
           {tiles.traditionalKingswayPark}
         </Column>
       </Row>
