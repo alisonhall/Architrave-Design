b962620fe6aff8f211fee39fc7e60ad98ccbb521
Alison Hall <alisonh0101@gmail.com>
2021-06-05 17:45:30 -0400

    Add icons to the Contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import EmailIcon from '../images/email-icon.svg';
+import PhoneIcon from '../images/phone-icon.svg';
 import '../scss/_contact.scss';
 
 const Contact = (props) => (
@@ -10,12 +12,24 @@ const Contact = (props) => (
     <section className="contentWrapper clearfix">
       <div className="contentBackground clearfix">
         <section className="textBlurb">
-          <p><a href="mailto:architravedesign@rogers.com"><i className="fa fa-envelope"></i>architravedesign@rogers.com</a></p>
-          <p><i className="fa fa-phone"></i>Toronto: <a href="tel:4162078881">416-207-8881</a></p>
-          <p><i className="fa fa-phone"></i>Mississauga & Oakville: <a href="tel:9055993097">905-599-3097</a></p>
+          <div className="item email">
+            <img className="icon email-icon" src={EmailIcon} alt="" />
+            <p><a href="mailto:architravedesign@rogers.com"><i className="fa fa-envelope"></i>architravedesign@rogers.com</a></p>
+          </div>
+          <div className="item phone">
+            <img className="icon phone-icon" src={PhoneIcon} alt="" />
+            <p>Toronto: <a href="tel:4162078881">416-207-8881</a></p>
+          </div>
+          <div className="item phone">
+            <img className="icon phone-icon" src={PhoneIcon} alt="" />
+            <p>Mississauga & Oakville: <a href="tel:9055993097">905-599-3097</a></p>
+          </div>
         </section>
       </div>
     </section>
+    <p className="visually-hidden">
+      Icon by <a href="https://freeicons.io/profile/3">freeicons</a> on <a href="https://freeicons.io">freeicons.io</a>
+    </p>
   </Layout>
 )
 
